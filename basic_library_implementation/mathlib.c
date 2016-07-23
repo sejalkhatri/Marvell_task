49ebc936f68d1d56cded5249ea25feaecf020ac3
zubokprevails <urjeeta.tule@gmail.com>
2016-07-23 17:00:05 +0530

    Update mathlib.c

--- basic_library_implementation/mathlib.c
+++ basic_library_implementation/mathlib.c
@@ -20,3 +20,57 @@ int div(int a, int b)
 	else
 		return a/b;
 }
+
+double exp(double a)
+{
+double f=1.00000;
+
+while(a>0)
+{f*=e;
+a--;}
+
+return f;
+}
+
+double pow(double a,double b)
+{
+double f=1.00000;
+
+while(b>0)
+{f*=a;
+b--;
+}
+
+return f;
+}
+
+double factorial(double a)
+{
+if(a==1)
+	return 1;
+else
+	return a*factorial(a-1);
+}
+
+double cos(double a)
+{
+double c=1;
+int i=1;
+for(;i<=5;i++)
+{
+c+=((pow(-1,i)*pow(a,2*i))/factorial(2*i));
+}
+return c;
+}
+
+double sin(double a)
+{
+double c=a;
+int i=1;
+for(;i<=5;i++)
+{
+c+=((pow(-1,i)*pow(a,((2*i)+1)))/factorial((2*i)+1));
+}
+return c;
+
+}
