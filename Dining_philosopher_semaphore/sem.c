a920d5aa2148521876b6ebcdef7df4a8611320a2
Sejal Khatri <sejal.khatri5@gmail.com>
2016-07-24 21:03:07 +0530

    Update sem.c

--- Dining_philosopher_semaphore/sem.c
+++ Dining_philosopher_semaphore/sem.c
@@ -83,26 +83,3 @@ int main()
 	}
 	return 0;
 }
-/*
-void *foo(void *i) {
-    int a = *((int *) i);
-    free(i);
-}
-
-int main() {
-    pthread_t thread;
-    for ( int i = 0; i < 10; ++1 ) {
-        int *arg = malloc(sizeof(*arg));
-        if ( arg == NULL ) {
-            fprintf(stderr, "Couldn't allocate memory for thread arg.\n");
-            exit(EXIT_FAILURE);
-        }
-
-        *arg = i;
-        pthread_create(&thread, 0, foo, arg);
-    }
-
-     Wait for threads, etc  
-
-    return 0;
-}*/
